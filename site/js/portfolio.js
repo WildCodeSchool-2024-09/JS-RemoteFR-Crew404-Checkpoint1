2ee420f2a8627a8404b202355cf325a22ee70d95
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-10-21 16:13:48 +0200

    step 3 JS done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -9,9 +9,24 @@ IMG.addEventListener("click", () => {
 
 const BUTTON_NAME = document.querySelector("#modifyName");
 const FIRSTNAME = document.querySelector("#firstname");
+const PINK_BGS = document.querySelectorAll(".pink-bg");
 
+console.log(PINK_BGS);
+
+// #750ff7
 BUTTON_NAME.addEventListener("click", () => {
 	const NAME = prompt("New name 💃");
+	const COLOR = prompt("New color (hex only)", "#750ff7");
+
 	FIRSTNAME.style.color = "white";
 	FIRSTNAME.textContent = NAME;
+
+	PINK_BGS.forEach((element) => {
+		element.style.backgroundColor = COLOR;
+	});
+
+	/** Une autre version d'une boucle */
+	// for (const BG of PINK_BGS) {
+	// 	BG.style.backgroundColor = COLOR;
+	// }
 });
