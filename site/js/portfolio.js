82712e79b1fee4c087c1bd7cb733c38e559d9f75
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-10-21 16:22:59 +0200

    step 5 🎁 JS done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -11,6 +11,7 @@ const BUTTON_NAME = document.querySelector("#modifyName");
 const FIRSTNAME = document.querySelector("#firstname");
 const PINK_BGS = document.querySelectorAll(".pink-bg");
 const PINK_TXT = document.querySelectorAll(".pink-text");
+const ALL_LINKS = document.querySelectorAll("a");
 
 // #750ff7
 BUTTON_NAME.addEventListener("click", () => {
@@ -27,6 +28,10 @@ BUTTON_NAME.addEventListener("click", () => {
 	for (const TEXT of PINK_TXT) {
 		TEXT.style.color = COLOR;
 	}
+
+	ALL_LINKS.forEach((link) => {
+		link.style.color = COLOR;
+	});
 
 	/** Une autre version d'une boucle */
 	// for (const BG of PINK_BGS) {
