685c7c276334fe56b9433cd00a4809fa5eba5e37
Antoine227 <antoine_chauvot@hotmail.com>
2024-10-21 11:51:14 +0200

    portfolio stepJS 2 ok

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,12 @@ bannerImg.addEventListener('click', () => {
     bannerImg.src = "./image/avatar.svg";
   });
 
+  const firstname = document.querySelector('#firstname');
+  const modifyButton = document.querySelector('#modify');
+
+  modifyButton.addEventListener('click', () => {
+    firstname.innerHTML = "Antonio Delavega";
+    firstname.style.color = "white";
+  });
+
+  
