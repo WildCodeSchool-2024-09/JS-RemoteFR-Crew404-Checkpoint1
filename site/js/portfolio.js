47f8a9a40906b8138f558c44d7477be9ec7fa2b1
Charles Catto <charlescatto@outlook.fr>
2024-10-21 12:20:10 +0200

    Last race changing JS.

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -9,31 +9,56 @@ function changePic() {
   changePic();
 
 
-  function changingTextAndColor () {
-    const name = document.querySelector('#firstname');
-    const promptModify = document.querySelector('#modify');
-    const bgColor = document.querySelectorAll('.bgName');
+//   function changingTextAndColor () {
+//     const name = document.querySelector('#firstname');
+//     const promptModify = document.querySelector('#modify');
+//     const bgColor = document.querySelectorAll('.bgName');
 
-    promptModify.addEventListener('click', () => {
-        name.style.color = 'white';
-        name.innerText = prompt('What is you name ?');
-        bgColor.style.backgroundColor = prompt('Wich color do you want ?');        
-    })
+//     promptModify.addEventListener('click', () => {
+//         name.style.color = 'white';
+//         name.innerText = prompt('What is you name ?');
+//         bgColor.style.backgroundColor = prompt('Wich color do you want ?');        
+//     })
 
-  }
+//   }
 
-  changingTextAndColor();
+//   changingTextAndColor();
 
-  function popUp() {
-    // Logique Serpentard
-    document.querySelector('#Serpentard').addEventListener('click', () => {
-      gameContext.theme = 'Serpentard'; // Sauvegarde tu thème.
+//   function popUp() {
+//     // Logique Serpentard
+//     document.querySelector('#Serpentard').addEventListener('click', () => {
+//       gameContext.theme = 'Serpentard'; // Sauvegarde tu thème.
     
-      for (const element of document.querySelectorAll('.customizable')) {
-        element.classList.add(gameContext.theme); // Application du thème.
+//       for (const element of document.querySelectorAll('.customizable')) {
+//         element.classList.add(gameContext.theme); // Application du thème.
+//       }
+//       for (const element of document.querySelectorAll('.customizable')) {
+//         element.classList.remove('Serdaigle', 'Gryffondor', 'Poufsouffle');
+//       }
+//     })
+// }
+
+    // const bgNamesC = document.querySelectorAll('.bgNameC');
+
+    function changingTextAndColor () {
+        const name = document.querySelector('#firstname');
+        const promptModify = document.querySelector('#modify');
+        const bgNames = document.querySelectorAll('.bgName');
+        const bgNamesC = document.querySelectorAll('.bgNameC');
+    
+        promptModify.addEventListener('click', () => {
+            name.style.color = 'white';
+            name.innerText = prompt('What is you name ?');
+            const wantedColor = prompt('Wich color do you want ?');
+            for (const bgName of bgNames) {
+              bgName.style.backgroundColor = wantedColor;
+            }
+        })
+        for (const bgName of bgNames) {
+            bgName.style.backgroundColor = wantedColor;
+          }
+        
+    
       }
-      for (const element of document.querySelectorAll('.customizable')) {
-        element.classList.remove('Serdaigle', 'Gryffondor', 'Poufsouffle');
-      }
-    })
-}
+    
+      changingTextAndColor();
