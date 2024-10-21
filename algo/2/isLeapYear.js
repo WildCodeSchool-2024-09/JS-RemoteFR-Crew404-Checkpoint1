da42cda2a6a27902f3064c078acbf904e68c95d1
Juloh-tuc <julie.tuco@gmail.com>
2024-10-21 10:11:43 +0200

    algo 2 done

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -8,7 +8,21 @@ Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utilise
 */
 
 function isLeapYear(year) {
-  // Your code here !
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ) {
+    return true;
+
+  } else {
+
+    return false;
+  }
 }
 
 module.exports = isLeapYear;
+
+let year = 2024
+if (isLeapYear(year)) {
+  console.log(year + "est une année bissextile."); 
+
+} else {
+  console.log(year + "n'est pas une année bisseextile."); 
+}
