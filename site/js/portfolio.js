921923a4a0f4ba71d24b1d46c77345cdd90820ae
Charles Catto <charlescatto@outlook.fr>
2024-10-21 12:11:51 +0200

    Further going into the JS things. Almost completed the last mandatory one.

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,5 @@
 function changePic() {
-    const img = document.querySelector('#avatarPic')
+    const img = document.querySelector('#avatarPic');
   
     img.addEventListener('click', () => {
       img.src = './image/avatar-bis.png'
@@ -7,3 +7,33 @@ function changePic() {
   }
 
   changePic();
+
+
+  function changingTextAndColor () {
+    const name = document.querySelector('#firstname');
+    const promptModify = document.querySelector('#modify');
+    const bgColor = document.querySelectorAll('.bgName');
+
+    promptModify.addEventListener('click', () => {
+        name.style.color = 'white';
+        name.innerText = prompt('What is you name ?');
+        bgColor.style.backgroundColor = prompt('Wich color do you want ?');        
+    })
+
+  }
+
+  changingTextAndColor();
+
+  function popUp() {
+    // Logique Serpentard
+    document.querySelector('#Serpentard').addEventListener('click', () => {
+      gameContext.theme = 'Serpentard'; // Sauvegarde tu thème.
+    
+      for (const element of document.querySelectorAll('.customizable')) {
+        element.classList.add(gameContext.theme); // Application du thème.
+      }
+      for (const element of document.querySelectorAll('.customizable')) {
+        element.classList.remove('Serdaigle', 'Gryffondor', 'Poufsouffle');
+      }
+    })
+}
