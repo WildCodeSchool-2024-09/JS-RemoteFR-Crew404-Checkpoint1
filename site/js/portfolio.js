c05a44084de176883973bb6800fea9233159f670
ElodieMons <emons47110@gmail.com>
2024-10-21 13:11:45 +0200

    CSS + essai JS

---
+++ site/js/portfolio.js
@@ -0,0 +1,16 @@
+const logo = document.querySelector('.Avatar');
+
+logo.addEventListener('click'), (e) => {
+    logo.src = "./site/image/logo-wcs.png";
+    logo.alt = "Image logo";
+}
+
+
+
+const form = document.querySelector("#firstname");
+
+firstname.addEventListener("click", function (event) {
+    console.log(event);
+
+});
+  
