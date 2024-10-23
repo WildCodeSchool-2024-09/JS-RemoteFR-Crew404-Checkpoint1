48e7432184c8009a4cb6a4454bb3bc4a3ee472ad
Manon Moutier <manon.moutier@hotmail.fr>
2024-10-23 19:26:17 +0200

    JS step 5 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -22,7 +22,8 @@ button.style.padding = "1rem";
 //récupération des conteneurs
 const wrapperName = document.getElementById('firstname');
 const backgrounds = document.querySelectorAll('.pink-bg');
-const texts=document.querySelectorAll('.pink-text')
+const texts = document.querySelectorAll('.pink-text')
+const liens = document.querySelectorAll('a');
 //Evenements au clic
 button.addEventListener('click', function () {
     const firstName = prompt("What's your name ?"); //Prénom en blanc
@@ -33,10 +34,16 @@ button.addEventListener('click', function () {
     for (const bg of backgrounds) {
         bg.style.background = bgColor;
     }
+
     button.style.background = bgColor;
+
     for (const txt of texts) {
         txt.style.color = bgColor;
         
+    }
+
+    for (const lien of liens) {
+        lien.style.color = bgColor;
     }
     
 });
